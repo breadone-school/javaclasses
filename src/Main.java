2e7b276a2fc85127231bcf2696b60c598f1460f1
PradyunS <pradyun.settI@gmail.com>
2020-05-28 17:05:11 +1200

    added main class added crit damage chance temp removed received damage

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    public static void main(String args[]) {
+        System.out.println("enter your character's name: ");
+        Player p1 = new Player(System.console().readLine());
+
+        
+
+    }
+
+}
--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,23 @@
+import java.util.Random;
+
 public class Player {
     String Name;
     int health = 100;
+    int dmg;
     
     Player(String name) {
         this.Name = name;
     }
+
+    public int Attack() {
+        Random rnd = new Random();
+
+        if (rnd.nextInt(50) > 35) {
+            return (dmg * 2);
+        } else {
+            return dmg;
+        }
+    }
+
+
 }
