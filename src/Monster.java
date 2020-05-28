f9621d2a9172cc2c49a41fc98815f3133c157dcf
PradyunS <pradyun.settI@gmail.com>
2020-05-28 17:14:38 +1200

    add monster class randomised player damage TODO: add attack and receive dmg for monsters and players

---
+++ src/Monster.java
@@ -0,0 +1,15 @@
+public class Monster {
+    int health;
+    int damageavg;
+
+    Monster(int health, int damageSeed) {
+        this.health = health;
+        this.damageavg = damageSeed;
+    }
+
+    public void attack(String player) {
+
+
+    }
+
+}
--- src/Player.java
+++ src/Player.java
@@ -1,17 +1,17 @@
 import java.util.Random;
 
 public class Player {
+    Random rnd = new Random();
+
     String Name;
     int health = 100;
-    int dmg;
+    int dmg = rnd.nextInt(20) + 5;
     
     Player(String name) {
         this.Name = name;
     }
 
     public int Attack() {
-        Random rnd = new Random();
-
         if (rnd.nextInt(50) > 35) {
             return (dmg * 2);
         } else {
@@ -19,5 +19,6 @@ public class Player {
         }
     }
 
+    
 
 }
